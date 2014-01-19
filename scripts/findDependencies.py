bbec67df0ccc377cee6024c4cc90b9a2f3b6e7eb
Siddhartha <git@sdht.in>
2014-01-19 22:00:51 +0530

    Update scripts and PKGBUILDs

--- scripts/findDependencies.py
+++ scripts/findDependencies.py
@@ -1,51 +1,71 @@
 import subprocess
-data=open("dependency-data-kf5-qt5","r").readlines()
-directdependencies={}
-reversedependencies={}
-for line in data:
-    line=line.strip(" \n").replace(" ","")
-    if line.find("frameworks/")==0:
-        parts=line.split(":")
-        if parts[0] not in directdependencies:
-            directdependencies[parts[0]]=set()
-        directdependencies[parts[0]].add(parts[1])
-        if parts[1] not in reversedependencies:
-            reversedependencies[parts[1]]=set()
-        reversedependencies[parts[1]].add(parts[0])
-
-fl=open("fdata1","w")
-text="digraph A {"
-for d in directdependencies:
-    fl.write(d+"\n")
-    fl.write(",".join(list(directdependencies[d]))+"\n\n")
-    for dn in directdependencies[d]:
-        text+='"%s"->"%s";\n' % (dn.split("/")[1],d.split("/")[1])
-text+="}"
-f = open("h1.txt","w")
-f.write(text)
-f.close()
-fl.close()
-
 import copy
-bk=copy.deepcopy(directdependencies)
 
-for framework in directdependencies:
-    if framework in reversedependencies:
-        for name in directdependencies[framework]:
-            for fname in reversedependencies[framework]:
-                if name in directdependencies[fname]:
-                    directdependencies[fname].remove(name)
+def findDependencies():
+    data=open("dependency-data-kf5-qt5","r").readlines()
+    directdependencies={}
+    reversedependencies={}
+    for line in data:
+        line=line.strip(" \n").replace(" ","")
+        if line.find("frameworks/")==0:
+            line=line.replace("frameworks/","")
+            parts=line.split(":")
+            if parts[0] not in directdependencies:
+                directdependencies[parts[0]]=set()
+            directdependencies[parts[0]].add(parts[1])
+            if parts[1] not in reversedependencies:
+                reversedependencies[parts[1]]=set()
+            reversedependencies[parts[1]].add(parts[0])
 
-fl=open("fdata2","w")
-text="digraph A {"
-for d in directdependencies:
-    fl.write(d+"\n")
-    fl.write(",".join(list(bk[d]))+"\n")
-    fl.write(",".join(list(directdependencies[d]))+"\n\n")
-    for dn in directdependencies[d]:
-        text+='"%s"->"%s";\n' % (dn.split("/")[1],d.split("/")[1])
-text+="}"
-f = open("h2.txt","w")
-f.write(text)
-f.close()
-fl.close()
+    fl=open("fdata1","w")
+    text="digraph A {"
+    for d in directdependencies:
+        fl.write(d+"\n")
+        fl.write(",".join(list(directdependencies[d]))+"\n\n")
+        for dn in directdependencies[d]:
+            text+='"%s"->"%s";\n' % (dn,d)
+    text+="}"
+    f = open("h1.txt","w")
+    f.write(text)
+    f.close()
+    fl.close()
+
+    bk=copy.deepcopy(directdependencies)
+
+    def removeDependencyFrom(fname, name):
+        if name in directdependencies[fname]:
+            directdependencies[fname].remove(name)
+
+    visited=set()
+    def searchDependencies(framework, f):
+        if framework in visited or framework not in bk:
+            return
+        visited.add(framework)
+        for name in bk[framework]:
+            for fname in reversedependencies[f]:
+                removeDependencyFrom(fname, name)
+            searchDependencies(name,f)
+
+    for framework in bk:
+        if framework in reversedependencies:
+            visited=set()
+            searchDependencies(framework, framework)
+
+
+    fl=open("fdata2","w")
+    text="digraph A {"
+    for d in directdependencies:
+        fl.write(d+"\n")
+        fl.write(",".join(list(bk[d]))+"\n")
+        fl.write(",".join(list(directdependencies[d]))+"\n\n")
+        for dn in directdependencies[d]:
+            text+='"%s"->"%s";\n' % (dn,d)
+    text+="}"
+    f = open("h2.txt","w")
+    f.write(text)
+    f.close()
+    fl.close()
+    return directdependencies
+
+if __name__ == '__main__':
+    findDependencies()
--- scripts/parseFrameworks.py
+++ scripts/parseFrameworks.py
@@ -1,59 +1,103 @@
 
+import findDependencies
+directdependencies=findDependencies.findDependencies()
+
 import xml.etree.ElementTree as ElementTree
 import os
 tree = ElementTree.parse('/home/lfiles/dev/sources/kde5/kde_projects.xml')
 root = tree.getroot()
 itemlist = root.find('component').findall('module')
 allitems=[]
+extradependencies={"frameworkintegration":"'kde5-attica-git'",
+                   "kauth":"'kde5-polkit-qt-git'",
+                   "kdewebkit":"'qt5-webkit'",
+                   "kde4support":"'qt5-tools'",
+                   "kdnssd-framework":"'avahi'",
+                   "kdoctools":"'docbook-xsl'",
+                   "kfileaudiopreview":"'kde5-phonon-qt5-git'",
+                   "khtml":"'kde5-phonon-qt5-git' 'giflib'",
+                   "kiconthemes":"'qt5-svg'",
+                   "kidletime":"'libxss'",
+                   "kimageformats":"'jasper' 'openexr'",
+                   "kio":"'krb5' 'qt5-script'",
+                   "kjobwidgets":"'qt5-x11extras'",
+                   "kjsembed":"'qt5-svg'",
+                   "knotifications":"'kde5-libdbusmenu-qt5-bzr'",
+                   "kpty":"'libutempter'",
+                   "kross":"'qt5-tools'",
+                   "kwindowsystem":"'libxfixes'",
+                   "kxmlgui":"'kde5-attica-git'",
+                   "solid":"'media-player-info' 'upower' 'udisks2'",
+                   "sonnet":"'enchant'",
+                   "knotifyconfig":"'kde5-phonon-qt5-git'"}
 for item in itemlist:
     name=item.attrib['identifier']
     data={"name":name}
     data['description']=item.find('description').text.strip()
     data['web']=item.find('web').text.strip()
     data['url']=item.find('repo').find('url').text.strip()
+    data['extra']=""
+    data['depend']="'qt5-base'"
+    if name in ["kglobalaccel", "kidletime" ,"kwindowsystem"]:
+        data['depend']="'qt5-x11extras'"
+    elif name in ["solid"]:
+        data['depend']="'qt5-declarative'"
+    if name in directdependencies:
+        data['depend']="'kf5-%s-git'" % "-git' 'kf5-".join(list(directdependencies[name]))
+    data['depend']+=" "+extradependencies[name] if name in extradependencies else ""
+    data['depend']=data['depend'].replace("kf5-kdesupport/attica","kde5-attica").replace("kf5-kdesupport/phonon/phonon","kde5-phonon-qt5")
+    if name in ['kdoctools']:
+        data['extra']="\noptions=('staticlibs')"
+    data['setpath']=""
+    if name in ['kconfigwidgets', 'kde4support', 'kded', 'kdesignerplugin' ,'kio' ,'kjsembed' ,'kross', 'kservice']:
+        data['setpath']='export XDG_DATA_DIRS="/opt/kf5/share:$XDG_DATA_DIRS"\n\n    '
+    data['extraoptions']=""
+    if name in ["solid"]:
+        data['extraoptions']=" \\\n        -DHUPNP_ENABLED=FALSE"
     allitems.append(data)
 
 base='''
 pkgname=kf5-[name]-git
 _pkgname=[name]
-pkgver=255
+pkgver=1
 pkgrel=1
 pkgdesc="[description]"
 arch=('i686' 'x86_64')
 url="[web]"
 license=('LGPL')
-depends=('qt5-base')
+depends=([depend])
 makedepends=('extra-cmake-modules-git' 'git')
-conflicts=("kf5-${_pkgname}" "${_pkgname}-git" "${_pkgname}")
-provides=("kf5-${_pkgname}" "${_pkgname}")
+group=("kf5")
+conflicts=("${_pkgname}-git")
+provides=("${_pkgname}-git")
 source=("[url]")
-md5sums=('SKIP')
+md5sums=('SKIP')[extra]
 
 pkgver() {
-  cd "${_pkgname}"
-  echo $(git rev-list --count HEAD).$(git rev-parse --short HEAD)
+    cd "${_pkgname}"
+    echo $(git rev-list --count HEAD).$(git rev-parse --short HEAD)
 }
 
 prepare() {
-  mkdir -p build
+    mkdir -p build
 }
 
 build() {
-  cd build
-  /opt/kf5/bin/cmake ../"${_pkgname}" \\
-    -DCMAKE_INSTALL_PREFIX=/opt/kf5 \\
-    -DCMAKE_BUILD_TYPE=DebugFull \\
-    -DLIB_INSTALL_DIR=lib
-  make
+    [setpath]cd build
+    /opt/kf5/bin/cmake ../"${_pkgname}" \\
+        -DCMAKE_INSTALL_PREFIX=/opt/kf5 \\
+        -DCMAKE_BUILD_TYPE=Debug \\
+        -DLIB_INSTALL_DIR=lib[extraoptions]
+    make
 }
 
 package() {
-  cd build
-  make DESTDIR="${pkgdir}" install
+    cd build
+    make DESTDIR="${pkgdir}" install
 }
 '''
 
-basepath = "/home/lfiles/dev/archlinux-PKGBUILDs/kf5-frameworks"
+basepath = "/home/lfiles/dev/archlinux-PKGBUILDs/kf5-others/a"
 
 for i in allitems:
     print("'kf5-%s-git'"%i['name'],end=' ')
