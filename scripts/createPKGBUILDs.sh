bbe380422f93da43828669ac4f4326addbd5d82c
Siddhartha <git@sdht.in>
2014-04-27 15:38:22 +0530

    Update scripts and PKGBUILDs

--- scripts/createPKGBUILDs.sh
+++ scripts/createPKGBUILDs.sh
@@ -5,7 +5,7 @@ python /home/lfiles/dev/scripts/parseFrameworks.py && \
 rm -rf /home/lfiles/dev/archlinux-logs/b && \
 mkdir -p /home/lfiles/dev/archlinux-logs/b && \
 python /home/lfiles/dev/scripts/parseWorkspace.py && \
-# rm -r /home/lfiles/dev/archlinux-PKGBUILDs/kf5/* && \
-# cp -a /home/lfiles/dev/archlinux-logs/a/* /home/lfiles/dev/archlinux-PKGBUILDs/kf5 && \
-# cp -a /home/lfiles/dev/archlinux-logs/manual/* /home/lfiles/dev/archlinux-PKGBUILDs/kf5 && \
+rm -r /home/lfiles/dev/archlinux-PKGBUILDs/kf5/* && \
+cp -a /home/lfiles/dev/archlinux-logs/a/* /home/lfiles/dev/archlinux-logs/b/* /home/lfiles/dev/archlinux-PKGBUILDs/kf5 && \
+cp -a /home/lfiles/dev/archlinux-logs/manual/* /home/lfiles/dev/archlinux-PKGBUILDs/kf5 && \
 echo "done."
