9f294720454e2e410b3ca54ddff6f7eb83a9c70b
Siddhartha <git@sdht.in>
2014-04-27 13:05:11 +0530

    Update scripts and PKGBUILDs

--- archlinux-install-scripts/7a-postreboot-xorg.sh
+++ archlinux-install-scripts/7a-postreboot-xorg.sh
@@ -1,6 +1,6 @@
 echo "Install xorg..." && \
 pacman -Syu --noconfirm \
             xorg-server xorg-server-utils xf86-input-synaptics \
-            mesa xf86-video-ati lib32-ati-dri \
+            mesa mesa-libgl xf86-video-ati lib32-ati-dri \
             xorg-xinit xorg-twm xorg-xclock xterm && \
 echo "Done. Execute 'startx' [after rebooting if in chroot] to test."
--- archlinux-install-scripts/o-uninstall-xorg-catalyst-hd234k.sh
+++ archlinux-install-scripts/o-uninstall-xorg-catalyst-hd234k.sh
@@ -10,12 +10,12 @@ sed -i '\|xorg112|b br
 echo "Remove catalyst installation..." && \
 pacman -Rdd catalyst-hook catalyst-utils catalyst-libgl opencl-catalyst lib32-catalyst-libgl lib32-opencl-catalyst lib32-catalyst-utils opencl-headers && \
 echo "Removing xorg config..." && \
-[[ -r /etc/X11/xorg.conf ]] && mv /etc/X11/xorg.conf /etc/X11/xorg.conf.bckupati || true && \
+rm /etc/X11/xorg.conf || true && \
 echo "Stopping catalyst service..." && \
 systemctl stop catalyst-hook && \
 systemctl disable catalyst-hook && \
 echo "Removing module config..." && \
-rm -f /etc/modprobe.d/blacklist-radeon.conf && \
+# rm -f /etc/modprobe.d/blacklist-radeon.conf && \
 rm -f /etc/modules-load.d/catalyst.conf && \
 echo "Removing nomodeset from kernel parameters..." && \
 sed -i "s/[ ]*nomodeset[ ]*/ /" /boot/grub/grub.cfg && \
